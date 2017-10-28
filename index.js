771c77d5aaea6dcb909b0aafec44396bae34fa92
Ben Lu <ben@ben-l.com>
2017-10-29 00:13:22 +1300

    Change some params

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function run(){
         
         const {lat,lng} = latLngDirCorrection(ll.lat, ll.lng, 35/180*Math.PI, 0)
         const {x,y} = getCircCoordHoriz(lat, lng, dim/2)
-        const coord = rotateCirc(x, y, dim/2, -3/180*Math.PI)
+        const coord = rotateCirc(x, y, dim/2, -6/180*Math.PI)
         //const coord = rotateCirc(i, j, dim/2, 0)
         for (var k = 0; k < 4; ++k) {
           im.set(i,j,k, pixels.get(parseInt(coord.x, 10)+xOffset,parseInt(coord.y, 10)+yOffset,k))
